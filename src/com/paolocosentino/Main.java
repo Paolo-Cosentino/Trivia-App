63bfd67b437b8cde76bb46c3dcf6c840faccdcb9
Paolo Cosentino <paolo@paolos-mbp.attlocal.net>
2020-05-17 22:59:42 -0700

    Add Question, Genre, Difficulty, Type Selector

--- src/com/paolocosentino/Main.java
+++ src/com/paolocosentino/Main.java
@@ -10,19 +10,14 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    public static ArrayList<Question> listOfQuestions = new ArrayList<>();
-
-    public static void main(String[] args) throws IOException, ParseException {
-        int numberOfQuestions;
-        int category;
-        String difficulty;
-        String questionType = "multiple";
-
-        URL url = new URL("https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple");
+    public static void main(String[] args) throws IOException, ParseException, InterruptedException {
+        String dbUrl = runPrompt();
+        URL url = new URL(dbUrl);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.connect();
@@ -44,6 +39,7 @@ public class Main {
         JSONParser parse = new JSONParser();
         JSONObject jobj = (JSONObject) parse.parse(inline.toString());
         JSONArray jsonarr = (JSONArray) jobj.get("results");
+        List<Question> listOfQuestions = new ArrayList<>();
 
         for (Object o : jsonarr) {
             JSONObject jsonobj = (JSONObject) o;
@@ -62,9 +58,10 @@ public class Main {
 
         /////////////////Game///////////////////
         int score = 0;
+        int count = 1;
         Scanner scan = new Scanner(System.in);
         for (Question q : listOfQuestions) {
-            System.out.println(q);
+            System.out.println(count + ". " + q);
             System.out.print("Pick an option: ");
             char userInput = scan.next().charAt(0);
 
@@ -87,10 +84,77 @@ public class Main {
             } else {
                 System.out.println("Incorrect, '" + q.getCorrectAnswer() + "' is the correct answer.");
             }
+            count++;
             System.out.println();
         }
 
         System.out.println("Score: " + score + "/" + listOfQuestions.size());
 
+    }
+
+    public static String runPrompt() {
+        Scanner scan = new Scanner(System.in);
+        StringBuilder str = new StringBuilder();
+        str.append("https://opentdb.com/api.php?");
+
+        System.out.print("Number of Questions (1-50): ");
+        int numOfQs = scan.nextInt();
+
+        while (numOfQs < 1 || numOfQs > 50) {
+            System.out.print("Number of Questions: ");
+            numOfQs = scan.nextInt();
+        }
+
+        str.append("amount=").append(numOfQs);
+
+        System.out.println("Category (0 = Any, 9-32): ");
+        int categoryOfQs = scan.nextInt();
+
+        while (categoryOfQs != 0 && categoryOfQs < 9 || categoryOfQs > 32) {
+            System.out.println("Category (0 = Any, 9-32): ");
+            categoryOfQs = scan.nextInt();
+        }
+
+        if (categoryOfQs != 0) {
+            str.append("&category=").append(categoryOfQs);
+        }
+
+        System.out.println("Difficulty (0. Any, 1. Easy, 2. Medium, 3. Hard): ");
+        int difficultyOfQs = scan.nextInt();
+
+        while (difficultyOfQs < 0 || difficultyOfQs > 3) {
+            System.out.println("Difficulty (0. Any, 1. Easy, 2. Medium, 3. Hard): ");
+            difficultyOfQs = scan.nextInt();
+        }
+
+        if (categoryOfQs != 0) {
+            String difficulty = switch (difficultyOfQs) {
+                case 1 -> "easy";
+                case 2 -> "medium";
+                case 3 -> "hard";
+                default -> "";
+            };
+            str.append("&difficulty=").append(difficulty);
+        }
+
+        System.out.println("Type (0. Any, 1. Multiple Choice, 2. True/False): ");
+        int typeOfQuestions = scan.nextInt();
+
+        while (typeOfQuestions < 0 || typeOfQuestions > 2) {
+            System.out.println("Type (0. Any, 1. Multiple Choice, 2. True/False): ");
+            typeOfQuestions = scan.nextInt();
+        }
+
+        if (typeOfQuestions != 0) {
+            String type = switch (typeOfQuestions) {
+                case 1 -> "multiple";
+                case 2 -> "boolean";
+                default -> "";
+            };
+            str.append("&type=").append(type);
+        }
+
+        System.out.println(str.toString());
+        return str.toString();
     }
 }
